d2058a8d53e1d44d0ad06c4256c220ac0c8ff1c5
Sandalot <20023123+Sandalot@users.noreply.github.com>
2024-01-04 22:48:06 +1100

    prints invite link when connected

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+    const invite = `https://discord.com/api/oauth2/authorize?client_id=${readyClient.user.id}&permissions=0&scope=bot%20applications.commands`
+    console.log(`Connected as ${readyClient.user.id}\n${invite}`);
 });
 
 client.login(config.DISCORD_TOKEN);
