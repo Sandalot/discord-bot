41332b58b0834a7f65c4601cbd1d85e1f475902c
Sandalot <20023123+Sandalot@users.noreply.github.com>
2024-01-04 23:00:18 +1100

    Add command handler from example docs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const fs = require('node:fs');
+const path = require('node:path');
 const config = require('./config.json');
-
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 client.once(Events.ClientReady, readyClient => {
     const invite = `https://discord.com/api/oauth2/authorize?client_id=${readyClient.user.id}&permissions=0&scope=bot%20applications.commands`
